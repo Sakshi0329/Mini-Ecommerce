eeb6a4d50ff3bd7ef9551b510f2519ff64cdd215
Sakshi0329 <sakshiparab0302@gmail.com>
2026-08-03 12:31:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,12 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:2600",
-      https://mini-ecommerce-frontend-git3.onrender.com
+      "https://mini-ecommerce-frontend-git3.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  }),
+  })
 );
-
 // Body Parsers
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
