665a8e06563fa2b6f8c84f979fe50d8c0ec3bded
Sakshi0329 <sakshiparab0302@gmail.com>
2026-08-04 18:49:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://mini-ecommerce-k6b66luho-sakshi-6726.vercel.app",
+      "https://mini-ecommerce-2ue3khnaw-sakshi-6726.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
